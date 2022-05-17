f1cdc360a98a98abb4d980812586a63fc30fbdf9
Siebe Geyskens <siebe.geyskens@student.ehb.be>
2022-05-17 15:31:28 +0200

    generate text based on image class and render - uses ml5 charRNN

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 let video;
-let flippedVideo;
 let imageModelURL = "https://teachablemachine.withgoogle.com/models/AwK6pw9qk/";
-
 let classifier;
-
+let rnn;
 let label = "waiting...";
+let button;
+let confidence;
+let seed;
+let genText;
 
 // STEP1: Load the model!
 // "preload" will load any important assets (img's, datafiles, models) before the program starts in setup
 function preload() {
   classifier = ml5.imageClassifier(imageModelURL);
+  rnn = ml5.charRNN(
+    "https://raw.githubusercontent.com/ml5js/ml5-data-and-models/main/models/charRNN/darwin/",
+    modelLoaded
+  );
+}
+
+function modelLoaded() {
+  console.log("Model Loaded!");
 }
 
 // connect to the capture device
 function setup() {
   let myCanvas = createCanvas(640, 480);
   myCanvas.parent("myContainer");
+
   // create a constraints object
   const constraints = {
     audio: false,
@@ -24,10 +35,11 @@ function setup() {
     },
   };
   video = createCapture(constraints);
-  video.size(320, 240); // WHY DOES THIS LINE MATTER?
   video.hide();
 
-  flippedVideo = ml5.flipImage(video);
+  button = createButton("write");
+  button.mousePressed(write);
+
   // STEP2: Start classifying
   classifyVideo();
 }
@@ -38,7 +50,7 @@ function draw() {
   background(0);
 
   // Draw the video
-  image(flippedVideo, 0, 0);
+  image(video, 0, 0);
   //   line(15, 25, 70, 90);
 
   // STEP 4: Draw the label
@@ -46,12 +58,31 @@ function draw() {
   textAlign(CENTER, CENTER);
   fill(255);
   text(label, width / 2, height - 17);
+  // To see if github pages updated:
 }
 
 function classifyVideo() {
-  flippedVideo = ml5.flipImage(video);
-  classifier.classify(flippedVideo, gotResults);
-  flippedVideo.remove();
+  classifier.classify(video, gotResults);
+}
+
+function write() {
+  seed = label;
+
+  rnn.generate(
+    {
+      seed: seed,
+      length: 50,
+      temperature: 0.5,
+    },
+    (err, results) => {
+      renderText(seed, results.sample);
+    }
+  );
+}
+
+function renderText(seed, genText) {
+  let p = createP(seed + "" + genText);
+  p.style("font-size", "16px");
 }
 
 // STEP3: Get the classification!
@@ -61,6 +92,7 @@ function gotResults(error, results) {
     return;
   }
   label = results[0].label;
+  confidence = results[0].confidence;
   classifyVideo();
 }
 
