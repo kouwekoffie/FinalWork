ed799e2ff0aedbab4eb457f1aa260994fe108561
Siebe Geyskens <siebe.geyskens@student.ehb.be>
2022-05-18 12:15:15 +0200

    added object detection example

---
+++ ObjectDetection/index.js
@@ -0,0 +1,53 @@
+let video;
+let detector;
+let detections = [];
+
+function preload() {
+  img = loadImage("./vetkruid.jpg");
+  detector = ml5.objectDetector("cocossd");
+}
+
+function drawDetections() {
+  //draw bounding boxes &labels (this is not where I got the detections but it's better to put this loop in draw then in gotDetections) - the global detections variable links the two loops
+  for (let i = 0; i < detections.length; i++) {
+    let object = detections[i];
+    stroke(0, 255, 0);
+    strokeWeight(4);
+    noFill();
+    rect(object.x, object.y, object.width, object.height);
+    noStroke();
+    fill(255);
+    text(object.label, object.x + 10, object.y + 20);
+  }
+}
+
+function gotDetections(error, results) {
+  if (error) {
+    console.error(error);
+  }
+  //   console.log(results);
+  detections = results;
+  detector.detect(video, gotDetections);
+}
+
+function detect() {
+  detector.detect(video, gotDetections);
+}
+
+function setup() {
+  createCanvas(640, 480);
+  const constraints = {
+    audio: false,
+    video: {
+      facingMode: "environment",
+    },
+  };
+  video = createCapture(constraints, detect);
+  video.size(640, 480); // same size as canvas
+  video.hide();
+}
+
+function draw() {
+  image(video, 0, 0);
+  drawDetections();
+}
